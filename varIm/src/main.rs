fe36f5b0df72c8888f776cb98fa0989b385af159
unknown <Devraj Singh>
2025-08-01 19:33:32 +0530

    today's work

--- varIm/src/main.rs
+++ varIm/src/main.rs
@@ -10,7 +10,6 @@ fn main() {
     const THREE_HOURS_INTO_SECONDS: u32 = 60 * 60 * 3;
     print!("three hours into seconds is {}", THREE_HOURS_INTO_SECONDS);
 }
-*/
 
 fn main() {
     let x = 5;
@@ -24,3 +23,43 @@ fn main() {
 
     print!("Outer scope x is {}", x);
 }
+
+
+fn main() {
+    let mut x = 4;
+    println!("before but no let : {}", x);
+    x = 34;
+    print!("{}", x);
+}
+
+fn main() {
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("total space_length is : {}", spaces);
+}
+
+
+fn main() {
+    let mut spaces = "   ";
+    spaces = spaces.len();
+    println1("bugged length is : {}", spaces);
+}
+
+use std::io;
+fn main() {
+    println!("input your guess : ");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to readline.");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("enter an integer");
+            return;
+        }
+    };
+    println!("guess is : {}", guess);
+}
+*/
+fn main() {}
